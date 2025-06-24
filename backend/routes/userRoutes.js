0c5dd22b669c6a775e8ecaf1277c693111ac41a8
GrahamBlackledge <gblackledge88@gmail.com>
2025-06-24 21:05:11 +1200

    fixed bug in userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express  = require('express');
 const router   = express.Router();
 const mongoose = require('mongoose');
 
-const User     = require('../models/User');
+const User     = require('../models/Users');
 const Country  = require('../models/Country');
 
 /* ──────────────────────────────────────────────────────────── */
